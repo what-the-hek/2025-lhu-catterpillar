97b150ff04c4b5918cc9c3e69296c3b0c3477c71
Jess Hek <108735805+what-the-hek@users.noreply.github.com>
2025-11-23 09:29:11 +0800

    background music

---
+++ Assets/Scripts/AudioManagerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    private static AudioManagerScript instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
